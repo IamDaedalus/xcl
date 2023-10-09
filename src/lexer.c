499c354df8971f32ede9ebdfda645755d48bc8b8
Daedalus <manny.is.daedalus@pm.me>
2023-10-09 16:05:11 +0000

    cleaned up lex_begin function

--- src/lexer.c
+++ src/lexer.c
@@ -9,12 +9,13 @@
 
 void lex_begin(char *file_buf, size_t size, Token **tokens, Error **err)
 {
+	(void)err;
 	size_t line = 1;
-	size_t i = 0;
+	size_t index = 0;
 
-	while (i < size)
+	while (index < size)
 	{
-		char c = file_buf[i];
+		char c = file_buf[index];
 
 		if (c == '\n')
 		{
@@ -23,105 +24,124 @@ void lex_begin(char *file_buf, size_t size, Token **tokens, Error **err)
 		}
 		else if (c == '#')
 		{
-			while (file_buf[i] != '\n')
-				i++;
+			while (file_buf[index] != '\n')
+				index++;
 		}
 		else if (is_alpha(c) != 0)
 		{
-			int start = i;
-
-			/* extract substr of the keyword while checking for overflow*/
-			while ((is_alpha(file_buf[i]) != 0) && i < size)
-				i++;
-
-			int kw_len = i - start;
-			char *kw = malloc(sizeof(char) * kw_len + 1);
-			if (!kw)
-			{
-				fprintf(stderr, "malloc failed at lex_begin\n");
-				exit(EXIT_FAILURE);
-			}
-
-			strncpy(kw, &file_buf[start], kw_len);
-			kw[kw_len] = '\0';
-
+			char *kw = lextract_id(file_buf, &index, size, line);
 			token_push(tokens, kw, line, check_identifier(kw));
 
 			free(kw);
-			i--;
+			index--;
 		}
 		else if (c == '"')
 		{
-			i++;
-			/* necessary for checking the ending quote */
-			int start = i;
-
-			/* TODO: escape values such as \" \' */
-			while (!(strchr("\t\n\"", file_buf[i])) && i < size)
-				i++;
-
-			if (file_buf[i] != '"')
-			{
-				err_push(err, "string unterminated at %lu", line);
-				return;
-			}
-
-			int str_len = i - start;
-			char *str = malloc(sizeof(char) * str_len + 1);
-			if (!str)
-			{
-				fprintf(stderr, "malloc failed at lex_begin\n");
-				exit(EXIT_FAILURE);
-			}
-
-			strncpy(str, &file_buf[start], str_len);
-			str[str_len] = '\0';
-
+			index++;
+			char *str = lextract_str(file_buf, &index, size, line);
 			token_push(tokens, str, line, TOK_STRING);
 
 			free(str);
-			i++;
+			index++;
 		}
 		else if (is_digit(c))
 		{
-			int start = i;
-
-			while (is_digit(file_buf[i]))
-			{
-				i++;
-			}
-			/* check if the last character after the last digit is a space
-			 * so that we can get a valid number
-			 */
-			if (file_buf[i] != ' ')
-			{
-				err_push(err, "invalid number detected at %lu\n", line);
-				return;
-			}
-
-			int num_len = i - start;
-			char *val = malloc(sizeof(char) * num_len + 1);
-			if (!val)
-			{
-				fprintf(stderr, "malloc failed in lex_begin\n");
-				exit(EXIT_FAILURE);
-			}
-
-			strncpy(val, &file_buf[start], num_len);
-			val[num_len] = '\0';
-
+			char *val = lextract_num(file_buf, &index, size, line);
 			token_push(tokens, val, line, TOK_NUMBER);
 
 			free(val);
 		}
-		i++;
+		index++;
 	}
+}
+
+char *lextract_id(char *file_buf, size_t *index, size_t size, size_t line)
+{
+	(void)line;
+	int start = *index;
+
+	/* extract substr of the keyword while checking for overflow*/
+	while ((is_alpha(file_buf[*index]) != 0) && *index < size)
+		(*index)++;
+
+	int kw_len = *index - start;
+	char *kw = malloc(sizeof(char) * kw_len + 1);
+	if (!kw)
+	{
+		fprintf(stderr, "malloc failed at lex_begin\n");
+		exit(EXIT_FAILURE);
+	}
+
+	strncpy(kw, &file_buf[start], kw_len);
+	kw[kw_len] = '\0';
+
+	return kw;
+}
+
+char *lextract_str(char *file_buf, size_t *index, size_t size, size_t line)
+{
+	/* necessary for checking the ending quote */
+	int start = *index;
+
+	/* TODO: escape values such as \" \' */
+	while (!(strchr("\t\n\"", file_buf[*index])) && *index < size)
+		(*index)++;
+
+	if (file_buf[*index] != '"')
+	{
+		fprintf(stderr, "no matching \" found for string on line %lu\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	int str_len = *index - start;
+	char *str = malloc(sizeof(char) * str_len + 1);
+	if (!str)
+	{
+		fprintf(stderr, "malloc failed at lex_begin\n");
+		exit(EXIT_FAILURE);
+	}
+
+	strncpy(str, &file_buf[start], str_len);
+	str[str_len] = '\0';
+
+	return str;
+}
+
+char *lextract_num(char *file_buf, size_t *index, size_t size, size_t line)
+{
+	int start = *index;
+
+	while (is_digit(file_buf[*index]) && *index < size)
+	{
+		(*index)++;
+	}
+	/* check if the last character after the last digit is a space
+	 * so that we can get a valid number
+	 */
+	if (file_buf[*index] != ' ')
+	{
+		fprintf(stderr, "invalid number at %lu\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	int num_len = *index - start;
+	char *val = malloc(sizeof(char) * num_len + 1);
+	if (!val)
+	{
+		fprintf(stderr, "malloc failed in lex_begin\n");
+		exit(EXIT_FAILURE);
+	}
+
+	strncpy(val, &file_buf[start], num_len);
+	val[num_len] = '\0';
+
+	return val;
 }
 
 TokenType check_identifier(char *identifier)
 {
 	int codes_len = 0, reg_len = 0;
-	int i = 0;
+	int index = 0;
 
 	char *opcodes[] = {
 		"mov", "add", "mul", "sub", "div", "mod",
@@ -137,22 +157,22 @@ TokenType check_identifier(char *identifier)
 	reg_len = sizeof(registers) / sizeof(registers[0]);
 
 	/* check the identifier against known opcodes */
-	while (i < codes_len)
+	while (index < codes_len)
 	{
-		if (strncmp(opcodes[i], identifier, strlen(identifier)) == 0)
+		if (strncmp(opcodes[index], identifier, strlen(identifier)) == 0)
 			return TOK_OPCODE;
 
-		i++;
+		index++;
 	}
-	i = 0; /* reset i for registers check */
+	index = 0; /* reset index for registers check */
 
 	/* check the identifier against known registers */
-	while (i < reg_len)
+	while (index < reg_len)
 	{
-		if (strncmp(registers[i], identifier, strlen(identifier)) == 0)
+		if (strncmp(registers[index], identifier, strlen(identifier)) == 0)
 			return TOK_REGISTER;
 
-		i++;
+		index++;
 	}
 
 	/* identifier is not a valid value */
--- src/lexer.h
+++ src/lexer.h
@@ -5,8 +5,11 @@
 #include "err.h"
 #include <stddef.h>
 
+char *lextract_str(char *file_buf, size_t *index, size_t size, size_t line);
+char *lextract_num(char *file_buf, size_t *index, size_t size, size_t line);
+char *lextract_id(char *file_buf, size_t *index, size_t size, size_t line);
+
 void lex_begin(char *file_buf, size_t size, Token **tokens, Error **err);
-char *lextract_text(char **buffer, size_t *cur_pos);
 TokenType check_identifier(char *identifier);
 
 #endif /* LEXER_H */
